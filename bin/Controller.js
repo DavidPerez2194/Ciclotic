4e5f239f0139454ed316808b6ee45f5d26c82ffd
DavidPerez2194 <dperezpayares@correo.unicordoba.edu.co>
2019-06-03 12:02:18 +0000

    .

--- bin/Controller.js
+++ bin/Controller.js
@@ -1,15 +1,19 @@
 const mongoose = require("mongoose");
-class Controller{
+class controller{
 constructor(){
     this.connect();
 }
 async connect(){
     try{
 await mongoose.connect(
-"mongodb+srv://davidperez:davidperez@cluster0-g616q.mongodb.net/Ciclotic?retryWrites=true&w=majority"
-)
+"mongodb+srv://davidperez:davidperez@cluster0-g616q.mongodb.net/Ciclotic?retryWrites=true&w=majority",
+{useNewUrlParser:true}
+);
+console.log("conectado a la base de datos")
         }  catch(e){
             console.error(e)
     }
 }
 }
+
+exports.controller = new controller()
