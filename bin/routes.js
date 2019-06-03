4faa95ac9f0a1a1cd0b428004dbd3168330e070b
DavidPerez2194 <50604934+DavidPerez2194@users.noreply.github.com>
2019-06-03 06:40:33 -0500

    Add files via upload

---
+++ bin/routes.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+
+
+app.get("/", (req, res) => {
+    res.send("hola mundo");
+});
+
+app.get("/usuarios", (req, res) => {
+  let usuarios = [
+    { id: "03", nombres: "david", apellido1: "gomez", apellido2: "lopez", edad: "30", sexo: "masculino", telefono: "312456382",
+    eps:"comfacor", email:"davideoeo210494@gmail.com" },
+      ];
+  res.send(usuarios);
+});
+
+exports.app = app;
