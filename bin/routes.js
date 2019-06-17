c509f9518a465fe596c2c58c6719495ffe0586d4
DavidPerez2194 <dperezpayares@correo.unicordoba.edu.co>
2019-06-17 12:30:23 +0000

    .

--- bin/routes.js
+++ bin/routes.js
@@ -2,6 +2,9 @@ const express = require("express");
 const app = express();
 const { controller } = require("./Controller");
 
+app.get("/", function(req, res) {
+  res.send("Ciclotic");
+});
 
 app.get("/usuarios", (req, res) => {
     controller.getUsers(res);
